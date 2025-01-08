d866d89cbb0978445b7db8c5dd969c582fa8ce68
Matthew Collins <m.collins@etoncollege.org.uk>
2025-01-08 22:43:35 +0000

    Initial Commit

---
+++ ViewLayout/ViewLayoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewLayoutApp.swift
+//  ViewLayout
+//
+//  Created by Collins, Matthew - MC on 08/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct ViewLayoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
